b5707a6e694d49dd40ec8afcb6ecf17f59c0343a
harrhunt <huntermakayharris@gmail.com>
2024-07-23 16:21:16 -0600

    moved function aliases to config file

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,18 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# ALIASES
+alias ls="lsd"
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+
+if type -q nvim
+    alias vim="nvim"
+end
+alias v="vim ."
+
 function starship_transient_prompt_func
   starship module character
 end
