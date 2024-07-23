855e1e12487481beef5cbf7408b29b91afb28f5a
harrhunt <huntermakayharris@gmail.com>
2024-07-23 09:30:59 -0600

    neorg adjustments

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -13,7 +13,7 @@ return {
         "j-hui/fidget.nvim",
     },
     config = function()
-        local cmp = require('cmp')
+        local cmp = require("cmp")
 
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
@@ -123,22 +123,22 @@ return {
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-f>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-f>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
 
             }),
             sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-
+                { name = "nvim_lsp" },
+                { name = "luasnip" }, -- For luasnip users.
+                { name = "neorg" },
             }, {
-                { name = 'buffer' },
+                { name = "buffer" },
             })
         })
 
--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -3,6 +3,37 @@ return {
         "nvim-neorg/neorg",
         lazy = false,
         version = "*",
-        config = true,
+        config = function()
+            require("neorg").setup({
+                load = {
+                    ["core.defaults"] = {},
+                    ["core.concealer"] = {
+                        config = {
+                            icon_preset = "diamond",
+                            icons = {
+                                todo = {
+                                    undone = {
+                                        icon = " ",
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    ["core.dirman"] = {
+                        config = {
+                            workspaces = {
+                                notes = "~/notes",
+                            },
+                            default_workspace = "notes",
+                        },
+                    },
+                    ["core.completion"] = {
+                        config = {
+                            engine = "nvim-cmp",
+                        },
+                    },
+                },
+            })
+        end,
     },
 }
