39a0b4a79415d4771e13c97e36ab9293d6831967
harrhunt <huntermakayharris@gmail.com>
2024-07-23 10:12:59 -0600

    added starship base config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+function starship_transient_prompt_func
+  starship module character
+end
+function starship_transient_rprompt_func
+  starship module time
+end
+starship init fish | source
+enable_transience
