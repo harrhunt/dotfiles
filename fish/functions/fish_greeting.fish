d4646068ec1cb3b49c108b07f52deeb8aeaacc3e
harrhunt <huntermakayharris@gmail.com>
2024-07-30 22:52:09 +0000

    added fetch to notifier

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,6 +1,7 @@
 function fish_greeting
     set cwd $(pwd)
     cd ~/.config
+    git fetch > /dev/null 2>&1
     set changes $(git rev-list --count origin/main...main)
     cd $cwd
     if test $changes -gt 0
