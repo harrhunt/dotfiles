1af30aa80465803196013d1592dd41031d09936c
harrhunt <huntermakayharris@gmail.com>
2024-07-30 17:03:26 +0000

    added notifier of config repo changes in remote

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,3 +1,12 @@
 function fish_greeting
+    set cwd $(pwd)
+    cd ~/.config
+    set changes $(git rev-list --count origin/main...main)
+    cd $cwd
+    if test $changes -gt 0
+        echo "|=================================================|"
+        echo "|   There are $changes new commits for dotfile config    |"
+        echo "|=================================================|"
+    end
 
 end
