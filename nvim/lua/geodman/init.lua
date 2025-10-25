0590a61325ae05aa75ab6c1b0dabe8fdab9fb2a5
harrhunt <huntermakayharris@gmail.com>
2025-10-25 16:28:22 -0600

    touched up lsp stuff

--- nvim/lua/geodman/init.lua
+++ nvim/lua/geodman/init.lua
@@ -1,10 +1,14 @@
 require("geodman.set")
 require("geodman.remap")
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
 local augroup = vim.api.nvim_create_augroup
 local geodmangroup = augroup('geodman', {})
 
 local autocmd = vim.api.nvim_create_autocmd
+
+vim.diagnostic.config({ jump = { float = true } })
 
 autocmd('LspAttach', {
     group = geodmangroup,
@@ -17,12 +21,14 @@ autocmd('LspAttach', {
         vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "<C-h>", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, opts)
         vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
         vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
     end,
 })
+
+vim.lsp.config("*", { capabilities = capabilities })
 
 vim.lsp.enable({
     "biome",
