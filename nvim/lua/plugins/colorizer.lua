3ea7ef69cee763a952880be6298e7664c83f7498
harrhunt <huntermakayharris@gmail.com>
2025-10-25 16:35:27 -0600

    switched color highlighter and added color picker

--- nvim/lua/plugins/colorizer.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-        require("colorizer").setup()
-        vim.keymap.set("n", "<leader>cc", "<cmd>ColorizerToggle<CR>",
-            { desc = "toggle color highlights in current buffer" })
-    end,
-}
