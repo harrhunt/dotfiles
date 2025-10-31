eaca97f6b477a9267b7ce041c3d3da019c2833d9
harrhunt <huntermakayharris@gmail.com>
2025-10-31 18:42:30 +0000

    trying no fetch

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,13 +1,12 @@
 function fish_greeting
-    set cwd $(pwd)
-    cd ~/.config
-    git fetch > /dev/null 2>&1
-    set changes $(git rev-list --count origin/main...main)
-    cd $cwd
-    if test $changes -gt 0
-        echo "╒══════════════════════════════════════════════════╕"
-        echo "│    There are $changes new commits for dotfile config    │"
-        echo "╘══════════════════════════════════════════════════╛"
-    end
-
+    # set cwd $(pwd)
+    # cd ~/.config
+    # git fetch > /dev/null 2>&1
+    # set changes $(git rev-list --count origin/main...main)
+    # cd $cwd
+    # if test $changes -gt 0
+    #     echo "╒══════════════════════════════════════════════════╕"
+    #     echo "│    There are $changes new commits for dotfile config    │"
+    #     echo "╘══════════════════════════════════════════════════╛"
+    # end
 end
