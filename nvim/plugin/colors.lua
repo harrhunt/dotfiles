888ab172c418790f8e6571d9f2113b84f3dafeae
harrhunt <huntermakayharris@gmail.com>
2026-04-25 09:37:56 -0600

    switch default to kanagawa ocean

--- nvim/plugin/colors.lua
+++ nvim/plugin/colors.lua
@@ -2,16 +2,18 @@
 -- vim.pack.add({ "https://github.com/rakr/vim-one" })
 -- vim.pack.add({ "https://github.com/vague-theme/vague.nvim" })
 vim.pack.add({ "https://github.com/rebelot/kanagawa.nvim" })
+-- vim.pack.add({ "https://github.com/srcery-colors/srcery-vim" })
 
-require("kanagawa").setup({
-    compile = true,
-    theme = "dragon",
-    background = {
-        dark = "dragon",
-        light = "lotus",
-    },
-})
+-- require("kanagawa").setup({
+--     compile = true,
+--     theme = "dragon",
+--     background = {
+--         dark = "dragon",
+--         light = "lotus",
+--     },
+-- })
 vim.cmd.colorscheme("kanagawa")
+-- vim.cmd.colorscheme("srcery")
 -- vim.cmd.colorscheme("vague")
 -- vim.cmd.colorscheme("rose-pine")
 -- vim.cmd.colorscheme("one")
